b07ac5fbbdf29951a2b8d52291e27faa91e70eec
sparkels36 <piyush362002@gmail.com>
2024-02-21 20:51:18 +0530

    project started and home page is added

---
+++ job/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import generic
+
+# Create your views here.
+
+
+class LandingPage(generic.TemplateView):
+    template_name = 'landing_page.html'
